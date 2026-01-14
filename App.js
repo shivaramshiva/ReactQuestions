022f198d7159db488a3bca8473f763bcdec051dd
shivaramshiva <m.shivaram02@gmail.com>
2026-01-14 12:12:18 +0530

    React Training Episode 1

---
+++ App.js
@@ -0,0 +1,42 @@
+const parent = React.createElement(
+    'div',
+    { id: 'parent' },
+    [
+        React.createElement(
+            'div',
+            { id: 'child' },
+            [
+                React.createElement(
+                    'h1',
+                    { id: 'heading' },
+                    'I am an h1 tag'
+                ),
+                React.createElement(
+                    'h2',
+                    { id: 'heading2' },
+                    'I am an h2 tag'
+                ),
+            ]
+        ),
+        React.createElement(
+            'div',
+            { id: 'child2' },
+            [
+                React.createElement(
+                    'h1',
+                    { id: 'heading' },
+                    'I am an h1 tag'
+                ),
+                React.createElement(
+                    'h2',
+                    { id: 'heading2' },
+                    'I am an h2 tag'
+                ),
+            ]
+        )
+    ]
+)
+const heading = React.createElement('h1', { id: 'heading', dataStyle: 'italics' }, "Hello World from React!!");
+console.log(parent); // Renderes the object 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent); // render method converts the object to h1 tag and fetches the output
