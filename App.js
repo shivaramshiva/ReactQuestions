344b70b0c9e0d6c804e4c88e8838aba78cdb309d
shivaramshiva <m.shivaram02@gmail.com>
2026-01-29 14:19:19 +0530

    JSX intro

--- App.js
+++ App.js
@@ -1,45 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement(
-    'div',
-    { id: 'parent' },
-    [
-        React.createElement(
-            'div',
-            { id: 'child' },
-            [
-                React.createElement(
-                    'h1',
-                    { id: 'heading' },
-                    'I am an h1 tag'
-                ),
-                React.createElement(
-                    'h2',
-                    { id: 'heading2' },
-                    'I am an h2 tag'
-                ),
-            ]
-        ),
-        React.createElement(
-            'div',
-            { id: 'child2' },
-            [
-                React.createElement(
-                    'h1',
-                    { id: 'heading' },
-                    'I am an h1 tag'
-                ),
-                React.createElement(
-                    'h2',
-                    { id: 'heading2' },
-                    'I am an h2 tag'
-                ),
-            ]
-        )
-    ]
-)
-const heading = React.createElement('h1', { id: 'heading', dataStyle: 'italics' }, "Hello World from React!!");
-console.log(parent); // Renderes the object 
+// Creating a heading using JSX
+// JSX - JavaScript XML => React.createElement => React Element (JS Object) => HTML Element (render)
+// THe above operation is done by Parcel using Babel compiler
+const jsxHeading = (<h1 id="heading" className="header-red">Hello World from React using JSX!!</h1>);
+console.log(jsxHeading); // Renderes the object
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent); // render method converts the object to h1 tag and fetches the output
+root.render(jsxHeading); // render method converts the object to h1 tag and fetches the output
